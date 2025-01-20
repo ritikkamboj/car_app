c4f7f7388d9e10152fd6a33a6eadce82aa77af53
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-01-20 14:41:53 +0530

    updated global css file and tailwind config file

---
+++ conponents/Hero.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">Find Book and Rent Car Easily </h1>
+        <p className="hero__subtitle">
+          Streamline your Car rental experience with our Effortless booking
+          Experience
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
