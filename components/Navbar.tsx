59995dcbea7692c153917399eaa33641f427a080
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-01-22 23:35:44 +0530

    working on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Navbar() {
   return (
-    <nav className="border-2 border-red-500 flexBetween padding-container max-container relative z-30 py-5"></nav>
+    <nav className="border-2 border-red-500 flexBetween padding-container max-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" width={79} height={29} alt="Logo" />
+      </Link>
+
+      <ul className="hidden h-full lg:flex gap-12">
+        {NAV_LINKS.map((item) => (
+          <Link href={`${item.href}${item.key}`} key={item.key}>
+            {item.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
   );
 }
 
