3c9094142c73742d0b2c3ce800770dfd27bada14
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-01-24 22:52:59 +0530

    working on camp section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Camp() {
-  return <div>Camp</div>;
+  return (
+    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:py-20 mb-10 xl:mb-20"></section>
+  );
 }
 
 export default Camp;
