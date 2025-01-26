16c975482368b191536fbe912111bbd86907cedb
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-01-26 10:16:27 +0530

    working on camp

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+function Campsite() {
+  return <div>Campsite</div>;
+}
+
 function Camp() {
   return (
-    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:py-20 mb-10 xl:mb-20"></section>
+    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:py-20 mb-10 xl:mb-20">
+      <div className="flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px]">
+        <Campsite backgroundImage="" title="" subtitle="" peopleJoined="" />
+      </div>
+    </section>
   );
 }
 
