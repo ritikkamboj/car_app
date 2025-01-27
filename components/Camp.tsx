813e62fcdb043e83be63aa1ebb227936bf09a07d
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-01-27 22:17:37 +0530

    wrong changes

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -7,7 +7,7 @@ function Campsite() {
 function Camp() {
   return (
     <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:py-20 mb-10 xl:mb-20">
-      <div className="flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px]">
+      <div className="flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px] so">
         <Campsite backgroundImage="" title="" subtitle="" peopleJoined="" />
       </div>
     </section>
