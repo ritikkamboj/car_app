d5c76c54fe041dbf9e6ca27715bc37fa27797044
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-01-29 11:26:26 +0530

    camp section completed

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -19,7 +19,7 @@ function Campsite({
     <div
       className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
     >
-      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10 border-2 border-blue-500">
+      <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10  ">
         <div className="flexCenter gap-4">
           <div className="rounded-full bg-green-50 p-4">
             <Image src="/folded-map.svg" alt="map" height={28} width={28} />
@@ -51,8 +51,8 @@ function Campsite({
 
 function Camp() {
   return (
-    <section className="border-2 border-green-500 2xl:max-container relative flex flex-col py-10 lg:py-20 mb-10 xl:mb-20">
-      <div className="flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px] border-2 border-red-400 overflow-x-auto">
+    <section className=" 2xl:max-container relative flex flex-col py-10 lg:py-20 mb-10 xl:mb-20">
+      <div className="flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px]  overflow-x-auto">
         <Campsite
           backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
@@ -65,6 +65,27 @@ function Camp() {
           subtitle="Somewhere in the Wilderness"
           peopleJoined="50+ Joined"
         />
+      </div>
+      <div className="flexEnd mt-10 px-6  lg:-mt-60 lg:mr-6">
+        <div className="flex-1 h-full w-full">jai shree ram</div>
+        <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full rounded-3xl overflow-hidden">
+          <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white">
+            <strong>Feeling Lost</strong> And Not Knowing the way?
+          </h2>
+          <p className="regular-14 xl:regular-16 mt-5 text-white">
+            Starting from the anxiety of the climbers when visiting a new
+            climbing location, the possibility of getting lost is very large.
+            That's why we are here for those of you who want to start an
+            adventure
+          </p>
+          <Image
+            src="/quote.svg"
+            alt="camp-2"
+            width={186}
+            height={219}
+            className="camp-quote"
+          />
+        </div>
       </div>
     </section>
   );
